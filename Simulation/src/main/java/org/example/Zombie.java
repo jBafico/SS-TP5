2cb22d750ba6386e0eccb84b52ec7c1e58369360
Agustin Z <86991224+azakalik@users.noreply.github.com>
2024-11-01 11:14:16 -0300

    Fixed zombie heuristic to avoid collisions between them

--- Simulation/src/main/java/org/example/Zombie.java
+++ Simulation/src/main/java/org/example/Zombie.java
@@ -19,12 +19,29 @@ public class Zombie extends Character{
         return new Zombie(coordinates, this.getConstants(), this.getConfig(), v, theta, r);
     }
 
+    private Zombie isCollidingWithZombie(List<Character> characterList) {
+        for (Character character : characterList) {
+            if (character instanceof Zombie && this.distanceToCollision(character) < 0) {
+                return (Zombie) character;
+            }
+        }
+        return null;
+    }
+
 
     @Override
     protected double getNextDesiredTheta(List<Character> characterList, Wall wall) {
         /* The zombie just considers the nearest human and doesn't avoid obstacles */
+        // Check if colliding with other zombie
+        Zombie collidingZombie = this.isCollidingWithZombie(characterList);
+        if (collidingZombie != null) {
+            // Calculate the angle between the two zombies and try to escape from the collision
+            double deltaX = collidingZombie.getX() - this.getX();
+            double deltaY = collidingZombie.getY() - this.getY();
+            return Math.atan2(deltaY, deltaX) + Math.PI;
+        }
 
-        // Find the nearest human
+        // Find the nearest human and follow him
         Character nearestHuman = this.findNNearestHumans(characterList, 1, getConfig().maxLengthFactor()).getFirst();
 
         // Calculate the difference in x and y coordinates
